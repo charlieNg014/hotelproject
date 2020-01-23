84dac3345c267f4eb40af6e2ebeb621dc0582832
charlieNg014 <cnguyen.swin@gmail.com>
2020-01-23 11:58:29 +0700

    Upload for testing

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -11,7 +11,7 @@ export default  class Contact extends Component {
         contacts: [
         {title:"Stay Connected", subtitle:"Monash University", details1: "14/201 Auburn Road, Hawthorn VIC 3122", details2: "0450 766 930", details3: "mngu0070@student.monash.edu"},
             {title:"Self-Introduction", subtitle: "Australian permanent resident and first year Monash software development student looking for an entry role or internship as a junior developer. Being a fast-learner, well-equipped knowledge in pogramming plus highly adaptable, I am expecting to participate in exciting projects to gain as much as possible hands-on experiences and contribute to the team.", details1: "", details2: "", details3: ""},
-            {title:"References", subtitle: "Nawfal Ali, PhD", details1: "Teaching Associate. Faculty of Information Technology, Monash University", details2: "", details3: ""}
+            {title:"References", subtitle: "CAN I HAVE YOUR NAME AS REFERENCE HERE?", details1: "Nawfal Ali, PhD Teaching Associate. Faculty of Information Technology, Monash University", details2: "", details3: ""}
         ]
     }
     render(){
